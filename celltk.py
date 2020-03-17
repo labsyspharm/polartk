86fcb4c900c91a672cf5350a0fa07c6f6e7c3b30
Hung-Yi Wu <hywu0110@gmail.com>
2020-03-17 13:22:33 -0400

    reorg and document celltk.py

--- celltk.py
+++ celltk.py
@@ -1,157 +1,169 @@
+import typing
+
 import numpy as np
+
 from scipy.ndimage import morphology
 from sklearn import neighbors
+from scipy import stats
 
-# for main function
-import matplotlib.pyplot as plt
-from skimage import measure, io, exposure
-from skimage.external import tifffile
+def polar_dist(
+    a: typing.Tuple[float, float],
+    b: typing.Tuple[float, float],
+    ):
+    '''
+    Distance metric in polar coordinate.
+    
+    Args
+        a, b: tuple of float
+            First element is radius. Second is angle in radian.
+        
+    Return
+        distance: float
+    '''
+    r1, t1 = a
+    r2, t2 = b
+    return np.sqrt(r1**2 + r2**2 - 2 * r1 * r2 * np.cos(t1 - t2))
 
-def xy2rt_v1(im, n_neighbors=5, new_shape=None, rel_distance=False, mask=None):
-    if new_shape is None:
-        new_shape = im.shape
-    # construct r (radius) and t (angle) grid
-    x, y = np.meshgrid(range(im.shape[0]), range(im.shape[1]), indexing='xy')
-    xc, yc = np.median(x), np.median(y)
-    if rel_distance:
-        _, r_inside = morphology.medial_axis(mask, return_distance=True)
-        r_inside = r_inside.max() - r_inside
-        _, r_outside = morphology.medial_axis(np.logical_not(mask), return_distance=True)
-        r = r_inside + r_outside
-    else:
-        r = np.sqrt((x-xc)**2 + (y-yc)**2)
-    t = np.arctan2(x-xc, y-yc)
-    def polar_dist(a, b):
-        r1, t1 = a
-        r2, t2 = b
-        return np.sqrt(r1**2 + r2**2 - 2 * r1 * r2 * np.cos(t1 - t2))
-    # KNN is more sensitive in dense area
-    # approximate by KNN regression
-    rt = np.stack([r.flatten(), t.flatten()], axis=-1)
-    model = neighbors.KNeighborsRegressor(n_neighbors=n_neighbors,
-            metric=polar_dist)
-    model.fit(rt, im.flatten())
-    # evaluate on new grid
-    new_r, new_t = np.meshgrid(
-            np.linspace(r.min(), r.max(), new_shape[0]),
-            np.linspace(t.min(), t.max(), new_shape[1]),
-            indexing='xy')
-    new_rt = np.stack([new_r.flatten(), new_t.flatten()], axis=-1)
-    new_im = model.predict(new_rt)
-    new_im = new_im.reshape(new_shape)
-    return new_im
-
-def xy2rt_v2(intensity_image, cell_centroid, nuclei_mask, cell_mask,
-        n_neighbors=5, new_shape=None):
-    # preparation
-    def polar_dist(a, b):
-        '''
-        distance metric in polar coordinate
-        '''
-        r1, t1 = a
-        r2, t2 = b
-        return np.sqrt(r1**2 + r2**2 - 2 * r1 * r2 * np.cos(t1 - t2))
-
-    # label, 0=background, 1=cytoplasm, 2=nuclei
-    label_xy = np.zeros_like(cell_mask, dtype=int)
-    label_xy[cell_mask] = 1
-    label_xy[nuclei_mask] = 2
-
+def xy2rt(
+    image: np.ndarray,
+    centroid: typing.Sequence[float]=None,
+    label: np.ndarray=None,
+    shape: typing.Tuple[int, int]=None,
+    params: typing.Dict[str, typing.Union[int, float]]=None,
+    ) -> typing.Sequence[np.ndarray]:
+    '''
+    Transform an image from xy (Euclidean) coordinate to rt (polar) coordinate.
+    
+    Args
+        image: numpy.ndarray
+            The intensity image to be transformed.
+        centroid: sequence of float, optional
+            Centroid, in xy coordinate, within the image to be used as the
+            origin of the polar coordinate. If not specified, the median
+            of the image dimension will be used.
+        label: numpy.ndarray, optional
+            Label (mask) of the image, 0=background, 1=cytoplasm, 2=nuclei.
+            If specified, the radius of the polar coordinate will be the relative
+            distance from the cell contour. If not specified, the radius will
+            be relative to the centroid.
+        shape: tuple of int, optional
+            Output shape. Used to generate the output grid for prediction.
+            If not specified, the shape of input ("image") will be used.
+            Can be larger than the shape of input, but depending on the
+            input resolution and application, the output resolution may
+            or may not make sense.
+        params: dict of str:number pairs, optional
+            Parameters, including:
+            pw: pad width, unit: pixel
+            pv: pad value for image
+            n_neighbors: params for KNN intensity model
+            
+    Return
+        r_grid: np.ndarray
+            Radius grid of the output, unit: pixel.
+        t_grid: np.ndarray
+            Angle grid of the output, unit: radian.
+        image_rt: np.ndarray
+            Transformed image.
+        label_rt: np.ndarray, if label is given
+            Transformed label.
+    '''
+    if params is None:
+        params = {
+            'pw': 1, # pad width, unit: pixel
+            'pv': 0, # pad value for image
+            'n_neighbors': 5, # params for KNN intensity model
+        }
+        
     # pad to remove boundary conditions
-    pw = 1 # 1 pixel
-    pv = 0 # 0=background
-    label_xy_pad = np.pad(label_xy, pad_width=pw, mode='constant', constant_values=pv)
-    intensity_image_pad = np.pad(intensity_image, pad_width=pw, mode='constant',
-            constant_values=pv)
-
-    # x, y, r (radius), theta (angle in radian)
+    image_pad = np.pad(image, pad_width=params['pw'], mode='constant',
+            constant_values=params['pv'])
+    if label is not None:
+        label_pad = np.pad(label, pad_width=params['pw'], mode='constant',
+                           constant_values=0) # 0=background
+        
+    # x, y
     x, y = np.meshgrid(
-            np.arange(label_xy_pad.shape[0]),
-            np.arange(label_xy_pad.shape[1]),
+            np.arange(image_pad.shape[0]),
+            np.arange(image_pad.shape[1]),
             indexing='ij')
-    xc, yc = cell_centroid[0]+pw, cell_centroid[1]+pw
-    r_nuclei = morphology.distance_transform_edt(label_xy_pad == 2)
-    r_nuclei = r_nuclei.max() - r_nuclei
-    r_cell = morphology.distance_transform_edt(label_xy_pad < 2)
-    r_background = morphology.distance_transform_edt(label_xy_pad == 0)
-    r = r_nuclei + r_cell + r_background
+    if centroid is None:
+        xc, yc = np.median(x), np.median(y)
+    else:
+        xc, yc = centroid
+    xc, yc = xc+params['pw'], yc+params['pw']
+    
+    # radius
+    if label is None:
+        r = np.sqrt((x-xc)**2 + (y-yc)**2)
+    else:
+        r_nuclei = morphology.distance_transform_edt(label_xy_pad == 2)
+        r_nuclei = r_nuclei.max() - r_nuclei
+        r_cell = morphology.distance_transform_edt(label_xy_pad < 2)
+        r_background = morphology.distance_transform_edt(label_xy_pad == 0)
+        r = r_nuclei + r_cell + r_background
+        
+    # angle (radian)
     t = np.arctan2(x-xc, y-yc)
+    
+    # approximate with KNN
     rt = np.stack([r.flatten(), t.flatten()], axis=-1)
-
-    # classification model: (R, Theta) --> label
-    label_model = neighbors.KNeighborsClassifier(n_neighbors=1, metric=polar_dist)
-    label_model.fit(rt, label_xy_pad.flatten())
-    intensity_model = neighbors.KNeighborsRegressor(n_neighbors=n_neighbors,
-            metric=polar_dist)
-    intensity_model.fit(rt, intensity_image_pad.flatten())
+    intensity_model = neighbors.KNeighborsRegressor(metric=polar_dist,
+                                                    n_neighbors=params['n_neighbors'])
+    intensity_model.fit(rt, image_pad.flatten())
+    if label is not None:
+        label_model = neighbors.KNeighborsClassifier(metric=polar_dist, n_neighbors=1)
+        label_model.fit(rt, label_pad.flatten())
 
     # create (R, Theta) grid
     # note that angle 2*pi == 0, so endpoint=False for angle
-    if new_shape is None:
-        new_shape = intensity_image_pad.shape
+    if shape is None:
+        shape = image.shape
     r_grid, t_grid = np.meshgrid(
-            np.linspace(start=r.min(), stop=r.max(), num=new_shape[0]),
-            np.linspace(start=0, stop=2*np.pi, num=new_shape[1], endpoint=False),
+            np.linspace(start=0, stop=r.max(), num=shape[0]),
+            np.linspace(start=0, stop=2*np.pi, num=shape[1], endpoint=False),
             indexing='ij')
-    rt_grid = np.stack([r_grid.flatten(), t_grid.flatten()], axis=-1)
-
-    # predict region and intensity
-    label_rt = label_model.predict(rt_grid).reshape(new_shape)
-    intensity_rt = intensity_model.predict(rt_grid).reshape(new_shape)
-
-    return label_rt, intensity_rt
-
-if __name__ == '__main__':
-    # load data
-    cell_mask = io.imread('../data/seg_masks/ZM131_10B_286_roi_A_cellRingMask.tif')
-    nuclei_mask = io.imread('../data/seg_masks/ZM131_10B_286_roi_A_nucleiRingMask.tif')
-    with tifffile.TiffFile('../data/images/ZM131_10B_286_roi_A_masked.ome.tif') as tif:
-        dna = tif.series[0].pages[0].asarray()
-        ps6 = tif.series[0].pages[21].asarray()
-
-    # get region
-    target = 6640
-    nuclei_region_list = measure.regionprops(label_image=nuclei_mask)
-    nuclei_region = [region for region in nuclei_region_list if region.label == target][0]
-    cell_region_list = measure.regionprops(label_image=cell_mask)
-    cell_region = [region for region in cell_region_list if region.label == target][0]
-
-    # unify coordinate to cell bounding box
-    cxl, cyl, cxu, cyu = cell_region.bbox
-    cm = cell_region.image
-    # nuclei
-    nxl, nyl, nxu, nyu = nuclei_region.bbox
-    nm = np.zeros_like(cm)
-    nm[(nxl-cxl):(nxu-cxl), (nyl-cyl):(nyu-cyl)] = nuclei_region.image
-    # cell centroid
-    cc = nuclei_region.centroid
-    cc = (cc[0]-cxl, cc[1]-cyl)
-    # markers
-    dna_i = dna[cxl:cxu, cyl:cyu]
-    ps6_i = ps6[cxl:cxu, cyl:cyu]
     
-    # test
-    label_rt, dna_rt = xy2rt_v2(intensity_image=dna_i, cell_centroid=cc, nuclei_mask=nm,
-            cell_mask=cm)
-    _, ps6_rt = xy2rt_v2(intensity_image=ps6_i, cell_centroid=cc, nuclei_mask=nm,
-            cell_mask=cm)
-
-    rgb_xy = np.zeros(dna_i.shape + (3,), dtype=float)
-    rgb_xy[..., 0] = exposure.rescale_intensity(dna_i.astype(float),
-            out_range=(0, 1),
-            in_range=tuple(np.percentile(dna_i, (1, 99))))
-    rgb_xy[..., 1] = exposure.rescale_intensity(ps6_i.astype(float),
-            out_range=(0, 1),
-            in_range=tuple(np.percentile(ps6_i, (1, 99))))
-
-    rgb_rt = np.zeros(dna_rt.shape + (3,), dtype=float)
-    rgb_rt[..., 0] = exposure.rescale_intensity(dna_rt.astype(float),
-            out_range=(0, 1),
-            in_range=tuple(np.percentile(dna_rt, (1, 99))))
-    rgb_rt[..., 1] = exposure.rescale_intensity(ps6_rt.astype(float),
-            out_range=(0, 1),
-            in_range=tuple(np.percentile(ps6_rt, (1, 99))))
-
-    plt.subplot(121); plt.imshow(rgb_xy); plt.title('xy')
-    plt.subplot(122); plt.imshow(rgb_rt); plt.title('rt')
-    plt.tight_layout(); plt.show()
+    # predict region and intensity
+    rt_grid = np.stack([r_grid.flatten(), t_grid.flatten()], axis=-1)
+    image_rt = intensity_model.predict(rt_grid).reshape(shape)
+    if label is not None:
+        label_rt = label_model.predict(rt_grid).reshape(shape)
+    
+    if label is None:
+        return r_grid, t_grid, image_rt
+    else:
+        return r_grid, t_grid, image_rt, label_rt
+    
+def polarity(d: typing.Sequence[float]):
+    '''
+    Custom definition of polarity.
+    
+    polarity := s/(s+1)
+    
+    s := cross entropy between the given angular distribution of intensity
+    and an uniform distribution.
+    
+    Cross entropy has range of [0, inf), and polarity defined here has
+    range of [0, 1). The transformation s/(s+1) is to make the output have
+    a more intuitive range.
+    
+    Args
+        d: sequence of float
+            Intensity at different angle (angular distribution).
+            
+    Return
+        polarity: float
+    '''
+    # handle nan
+    d = d[np.isfinite(d)]
+    # maek reference
+    dref = np.ones_like(d)
+    dref /= dref.sum()
+    # handle numerical stability
+    d2 = d + 1e-6
+    d2 /= d2.sum()
+    # polarity = s/(s+1), s = cross entropy
+    s = stats.entropy(dref, d2) # range [0, inf)
+    p = s/(s+1) # range [0, 1)
+    return p
