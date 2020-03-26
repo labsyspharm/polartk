beafb178d05862c44b2bcb05cbbb766153982c4d
hywu0110 <hywu0110@gmail.com>
2020-03-26 17:24:56 -0400

    added arrows

--- code/fig2/gen_fig2a.py
+++ code/fig2/gen_fig2a.py
@@ -8,6 +8,24 @@ import matplotlib.colorbar as mcolorbar
 import matplotlib.cm as mcolormap
 import matplotlib.colors as mcolors
 
+from matplotlib.legend_handler import HandlerPatch
+
+class HandlerArrow(HandlerPatch):
+    '''
+    For showing an arrow in a custom legend.
+
+    Source:
+    https://stackoverflow.com/questions/60781312/plotting-arrow-in-front-of-legend-matplotlib
+    '''
+    def create_artists(self, legend, orig_handle, xdescent, ydescent, width,
+            height, fontsize, trans):
+        p = mpatches.FancyArrowPatch(posA=(-9, 2), posB=(15, 2),
+                mutation_scale=8,
+                color='magenta', arrowstyle='<->')
+        self.update_prop(p, orig_handle, legend)
+        p.set_transform(trans)
+        return [p]
+
 if __name__ == '__main__':
     # paths
     data_folderpath = '.'
@@ -37,26 +55,43 @@ if __name__ == '__main__':
     original_label_xy = xydata_dict['nxcx'][..., 0]
     axes[1].imshow(original_label_xy, cmap='tab10')
     axes[1].set_title('mask (Euclidean)', fontsize=8)
+    axes[1].annotate(s='', xy=(18, 16), xytext=(18+5, 16-5),
+            arrowprops=dict(arrowstyle='<->', color='magenta'))
+    axes[1].annotate(s='', xy=(18, 9), xytext=(18+5, 9-5),
+            arrowprops=dict(arrowstyle='<->', color='magenta'))
 
     original_label_rt = rtdata_dict['nxcx'][..., 0]
     axes[2].imshow(original_label_rt, cmap='tab10')
     axes[2].set_title('mask (polar)', fontsize=8)
+    axes[2].annotate(s='', xy=(13.5, 5), xytext=(13.5+0, 5+5*np.sqrt(2)),
+            arrowprops=dict(arrowstyle='<->', color='magenta'))
+    axes[2].annotate(s='', xy=(20, 0), xytext=(20+0, 0+5*np.sqrt(2)),
+            arrowprops=dict(arrowstyle='<->', color='magenta'))
 
     for ax in axes[0:3]:
         ax.set_xticks([])
         ax.set_yticks([])
 
+    tgrid_list = []
     tdist_list = []
+    full_tgrid = np.linspace(-np.pi, np.pi, num=30, endpoint=False)
+    full_tgrid = np.degrees(full_tgrid)
+
     for rtdata in rtdata_dict.values():
-        tdist = rtdata[..., 1].sum(axis=0) # (R, T) -> (T,)
+        cytoplasm_mask = rtdata[..., 0] == 1
+        intensity = rtdata[..., 1]
+        tdist = np.multiply(intensity, cytoplasm_mask).sum(axis=0)
+        valid_mask = cytoplasm_mask.sum(axis=0) > 0
+        tdist = tdist[valid_mask]
+        tgrid = full_tgrid[valid_mask]
+
         tdist /= tdist.sum() # normalize total expression abundance
+
+        tgrid_list.append(tgrid)
         tdist_list.append(tdist)
 
-    t_grid = np.linspace(-np.pi, np.pi, num=tdist_list[0].shape[0], endpoint=False)
-    t_grid = np.degrees(t_grid)
-
-    for tdist in tdist_list:
-        axes[3].plot(t_grid, tdist, 'k-', alpha=0.1)
+    for tgrid, tdist in zip(tgrid_list, tdist_list):
+        axes[3].plot(tgrid, tdist, 'k-', alpha=0.1)
 
     axes[3].set_yticks([])
     axes[3].set_xticks(np.linspace(-180, 180, num=3))
@@ -64,6 +99,9 @@ if __name__ == '__main__':
     axes[3].set_title('angular distributions', fontsize=8)
     axes[3].set_xlabel('angle (degrees)', fontsize=8)
     axes[3].set_ylabel('normalized intensity', fontsize=8)
+
+    axes[3].annotate(s='', xy=(-20, 0.027), xytext=(-20, 0.027+0.025),
+            arrowprops=dict(arrowstyle='<->', color='magenta'))
 
     asp = np.diff(axes[3].get_xlim())[0] / np.diff(axes[3].get_ylim())[0]
     axes[3].set_aspect(asp)
@@ -83,9 +121,13 @@ if __name__ == '__main__':
                 label='cytoplasm'),
             mpatches.Patch(facecolor='tab:blue', edgecolor='tab:blue',
                 label='environment'),
+            mpatches.FancyArrowPatch(posA=(0, 0), posB=(5, 0), arrowstyle='<->',
+                label='perturbation directions', color='magenta'),
             ]
-    plt.figlegend(handles=legend_elements, ncol=3, bbox_to_anchor=(0.5, 0.11),
-            loc='lower center', fontsize=6)
+    plt.figlegend(handles=legend_elements, ncol=4, bbox_to_anchor=(0.5, 0.11),
+            loc='lower center', fontsize=6,
+            handler_map={mpatches.FancyArrowPatch : HandlerArrow()},
+            )
 
     # custom colorbar
     ax_colorbar = fig.add_axes([0.025, 0.15, 0.205, 0.03])
